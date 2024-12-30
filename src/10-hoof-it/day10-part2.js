14bd51237218e262e8f8ba304e1c5570bc325904
soenderstrup <mads.soenderstrup@gmail.com>
2024-12-30 16:11:30 +0100

    day10 p2

---
+++ src/10-hoof-it/day10-part2.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const FileReader = require('../util/FileReader')
+
+const filePath = path.join(__dirname, 'input.txt')
+const map = new FileReader()
+    .read(filePath)
+    .split('\n')
+    .map((line) =>
+        line
+            .trim()
+            .split('')
+            .map((char) => Number(char))
+    )
+
+function calcTrailRating(row, col, score = 0) {
+    if (map[row]?.[col] !== score) {
+        return 0
+    } else if (map[row][col] === 9 && score === 9) {
+        return 1
+    } else {
+        const nextScore = score + 1
+        return (
+            calcTrailRating(row, col + 1, nextScore) +
+            calcTrailRating(row, col - 1, nextScore) +
+            calcTrailRating(row + 1, col, nextScore) +
+            calcTrailRating(row - 1, col, nextScore)
+        )
+    }
+}
+
+console.time()
+let trailheadScores = 0
+for (let row = 0; row < map.length; row++) {
+    for (let col = 0; col < map[0].length; col++) {
+        if (map[row][col] === 0) {
+            trailheadScores += calcTrailRating(row, col)
+        }
+    }
+}
+
+console.log(trailheadScores)
+console.timeEnd()
